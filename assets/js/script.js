b8f16ea7356f30d589ba6f39fbcf253a01c172b7
bagl0025 <bagl0025@umn.edu>
2022-03-28 16:44:50 -0500

    added date picker

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -101,12 +103,20 @@ $(".list-group").on("click", "span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
-  // automatically focus on new element
+  // jquery ui datepicker
+  dateInput.datepicker({
+    // minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
+
+  // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -134,6 +144,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  //pass task's <li> element into auditTask()
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
   //make sortable
@@ -183,6 +196,32 @@ $(".list-group").on("blur", "input[type='text']", function() {
       ui.draggable.remove();
     }
   });
+
+  // add datepicker
+  $("#modalDueDate").datepicker({
+    // minDate: 1
+  });
+  
+  var auditTask = function(taskEl) {
+    // get date
+    var date = $(taskEl).find("span").text().trim();
+    console.log(date)
+
+    //convert to mement object
+    var time = moment(date, "L").set("hour", 17);
+    console.log(time);
+
+    // remove old classes
+    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+    // apply new class if task is near/over due date
+    if (moment().isAfter(time)) {
+      $(taskEl).addClass("list-group-item-danger");
+    }
+    else if (Math.abs(moment().diff(time, "days")) <= 2) {
+      $(taskEl).addClass("list-group-item-warning");
+    }
+  };
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
